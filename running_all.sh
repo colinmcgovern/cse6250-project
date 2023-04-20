2d1e9765f3d6dd845bd807ea46267a1b87e850ca
colin mcgovern <colinmcg@udel.edu>
2023-04-20 14:40:59 -0400

    nearly complete, but missing cf

--- running_all.sh
+++ running_all.sh
@@ -1,5 +1,18 @@
-python main.py 200 10 CNN;
-python main.py 200 10 SVM-L;
-python main.py 200 10 SVM-RBF;
-python main.py 200 10 RF;
-python main.py 200 10 FFNN;
+python main.py 350 30 CNN 5;
+python main.py 350 30 SVM-L 5;
+python main.py 350 30 SVM-RBF 5;
+python main.py 350 30 RF 5;
+
+python main.py 350 30 CNN 4;
+python main.py 350 30 SVM-L 4;
+python main.py 350 30 SVM-RBF 4;
+python main.py 350 30 RF 4;
+
+python main.py 350 30 CNN 3+1;
+python main.py 350 30 SVM-L 3+1;
+python main.py 350 30 SVM-RBF 3+1;
+python main.py 350 30 RF 3+1;
+
+python main.py 350 30 FFNN 5;
+python main.py 350 30 FFNN 4;
+python main.py 350 30 FFNN 3+1;
